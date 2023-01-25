9f55609df329aa9afee95499b5638ecac5df81be
EREKLE GVELESIANI <Vinshencoo@gmail.com>
2023-01-25 16:21:34 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+<script>
+export default{
+    data(){
+        return{
+            menuList: ['Home', 'Products', 'About Us', 'Contact']
+        }
+    },
+}
 </script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! test" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  <div class="div1">
+      <ul>
+          <li v-for="item in menuList" :key="item.index" >{{item}}</li>
+      </ul>
+  </div>
+  </template>
+  <style scoped>
+  div {
+      border: rgb(242, 242, 242) 1px solid;
+      background-color: rgb(0, 0, 0);
+  }
+  ul {
+      display:flex;
+      gap: 100px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  </style>
